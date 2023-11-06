b3b28e7c48e044ad6bd39766f3fd4f6156ac7f8f
dylan-goetting <76505830+dylan-goetting@users.noreply.github.com>
2023-11-05 16:37:45 -0800

    Update sample_contract.sol

--- sample_contract.sol
+++ sample_contract.sol
@@ -1,3 +1,18 @@
 contract MyContract {
+    uint public myVar;
 
+    event MyVarChanged(uint oldValue, uint newValue);
+
+    constructor(uint _myVar) {
+        myVar = _myVar;
+    }
+
+    function setMyVar(uint _myVar) public {
+        emit MyVarChanged(myVar, _myVar);
+        myVar = _myVar;
+    }
+
+    function getMyVar() public view returns (uint) {
+        return myVar;
+    }
 }
